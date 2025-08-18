2be07b411dad47f4534e2945f8baa9c1f306a500
Mahdi Amirikhamkani <mehdiamiri321@gmail.com>
2025-08-19 01:10:14 +0200

    Add Question class implementation and create empty Main, MathQuiz classes

---
+++ MathQuizGame-OOP/src/Question.java
@@ -0,0 +1,48 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Question {
+    private final Scanner scanner;
+    public Question(Scanner scanner){
+        this.scanner = scanner;
+    }
+
+    private static final char[] operators = {'+', '-' , '*', '/'};
+
+    private final Random num = new Random();
+
+    // create a random question
+    private Object[] generateQuestion(){
+        double num1 = num.nextDouble(1000);
+        double num2;
+        do{
+            num2 = num.nextDouble(1000);
+        } while (num2 == 0);
+
+        int operatorIndex = num.nextInt(4);
+        char operator = operators[operatorIndex];
+
+        // calculate answer
+        double answer = switch (operator){
+            case '+' -> num1 + num2;
+            case '-' -> num1 - num2;
+            case '*' -> num1 * num2;
+            case '/' -> Math.round((num1 / num2) * 100.0) / 100.0;
+            default -> 0;
+        };
+        return new Object[] {num1, num2, operator, answer};
+    }
+    // ask question and check answer
+    public boolean question(){
+        Object[] questionElements = generateQuestion();
+        double answer = (double) questionElements[3];
+
+        System.out.printf("%.2f %c %.2f = ", (double) questionElements[0], (char) questionElements[2], (double) questionElements[1]);
+        double userInput = scanner.nextDouble();
+
+        // tolerance
+        double epsilon = 1e-6;
+        return Math.abs(userInput - answer) < epsilon;
+    }
+
+}
