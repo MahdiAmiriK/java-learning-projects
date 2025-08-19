8a80288cef14e5e259d60a2161e903e63a17b57f
Mahdi Amirikhamkani <mehdiamiri321@gmail.com>
2025-08-20 01:55:21 +0200

    simplify dice class and add bonus statement for equal dices and place the conditions in a loop in Game class

--- DiceRollingGame-OOP/src/Dice.java
+++ DiceRollingGame-OOP/src/Dice.java
@@ -3,10 +3,9 @@ import java.util.Random;
 public class Dice {
 
     private Random random = new Random();
-    private int min = 1, max = 6;
 
     public int diceRolling(){
-        int diceNumber = random.nextInt(max - min +1) + min;
+        int diceNumber = random.nextInt(6) + 1;
         return diceNumber;
     }
 }
--- DiceRollingGame-OOP/src/Game.java
+++ DiceRollingGame-OOP/src/Game.java
@@ -1,3 +1,4 @@
+import java.sql.Array;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -16,40 +17,50 @@ public class Game {
     Dice dice1 = new Dice();
     Dice dice2 = new Dice();
 
+    private int[] rollDices(){
+        int diceNum1 = dice1.diceRolling();
+        int diceNum2 = dice2.diceRolling();
+        int[] result = {diceNum1,diceNum2};
+        return result;
+    }
+
     public void gameStart(){
         int targetScore = random.nextInt(11) + 2;
         System.out.println("Target score is " + targetScore);
-        System.out.println("To roll the dice press enter");
-        scanner.nextLine();
 
-        int diceNum1 = dice1.diceRolling();
-        int diceNum2 = dice2.diceRolling();
-        score = score + diceNum1 + diceNum2;
+        while (true){
+            System.out.println("To roll the dice press enter");
+            scanner.nextLine();
+            int[] dice = rollDices();
+            score = dice[0] + dice[1];
 
-        if(score == targetScore){
-            System.out.println("Congrats! You win the game.");
-            System.out.println("Target score was " + targetScore + " and you roll " + diceNum1 + " and " + diceNum2);
-        }else{
-            System.out.println("Oops! You lose.");
-            System.out.println("Target score was " + targetScore + " and you roll " + diceNum1 + " and " + diceNum2);
+            if(score == targetScore){
+                System.out.println("Congrats! You win the game.");
+                System.out.println("Target score was " + targetScore + " and you roll " + dice[0] + " and " + dice[1]);
+                break;
+            } else if (dice[0] == dice[1]) {
+                System.out.println("You roll a double (" + dice[0] + " and " + dice[1] + ") and you can roll again.");
+                continue;
+            } else{
+                System.out.println("Oops! You lose.");
+                System.out.println("Target score was " + targetScore + " and you roll " + dice[0] + " and " + dice[1]);
 
-            while (true){
-                System.out.print("Do you want to roll again? (yes/no) ");
-                String userDecision = scanner.nextLine();
-                if (userDecision.equalsIgnoreCase("yes")){
-                    score = 0;
-                    gameStart();
-                    break;
-                } else if (userDecision.equalsIgnoreCase("no")) {
-                    System.out.println("Game Over!");
-                    break;
-                } else {
-                    System.out.println("Invalid input");
+                while (true){
+                    System.out.print("Do you want to roll again? (yes/no) ");
+                    String userDecision = scanner.nextLine();
+                    if (userDecision.equalsIgnoreCase("yes")){
+                        score = 0;
+                        gameStart();
+                        break;
+                    } else if (userDecision.equalsIgnoreCase("no")) {
+                        System.out.println("Game Over!");
+                        break;
+                    } else {
+                        System.out.println("Invalid input");
+                    }
                 }
+
             }
-
         }
-
     }
-
 }
