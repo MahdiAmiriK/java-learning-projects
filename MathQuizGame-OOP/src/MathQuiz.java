68cf5e690144e7cd3b7627d909362b0c316349f8
Mahdi Amirikhamkani <mehdiamiri321@gmail.com>
2025-08-19 02:00:11 +0200

    Implement MathQuiz

--- MathQuizGame-OOP/src/MathQuiz.java
+++ MathQuizGame-OOP/src/MathQuiz.java
@@ -1,4 +1,27 @@
+import java.util.Scanner;
 
 public class MathQuiz {
+
+    private final Question q;
+
+    // Constructor: inject Scanner into Question
+    public MathQuiz(Scanner scanner){
+        this.q = new Question(scanner);
+    }
+
+    // Start the quiz with 5 questions
+    public int startQuiz(){
+        int score = 0;
+        for (int i = 1 ; i <= 5 ; i++){
+            boolean correct = q.question();
+            if (correct) {
+                System.out.println("Correct!");
+                score++;
+            } else {
+                System.out.println("Wrong!");
+            }
+        }
+        return score;
+    }
 
 }
