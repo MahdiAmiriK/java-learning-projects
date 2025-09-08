7f2a86151d91d80a07488d695f3afaec2ae6ee2c
Mahdi Amirikhamkani <mehdiamiri321@gmail.com>
2025-09-08 23:18:44 +0200

    Implement Main class

---
+++ PasswordValidator-ExceptionHandling/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter Password: ");
+        String password = scanner.nextLine();
+
+        System.out.print("Confirm Password: ");
+        String confirm = scanner.nextLine();
+
+        UserRegistration registration = new UserRegistration();
+        registration.registerUser(password, confirm);
+    }
+}
